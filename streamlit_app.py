f7a17996e5223097a4e7d076a17d9903cfd37b00
AbhishekPareek2005 <135148917+AbhishekPareek2005@users.noreply.github.com>
2023-06-01 05:24:14 +0530

    Update streamlit_app.py added the menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit
 streamlit.title('My parents new healthy dinner')
-
+streamlit.header('Breakfast Menu')
+streamlit.text('Masala Dosa')
+streamlit.text('Poha')
+stremlit.text('Aloo Parantha')
