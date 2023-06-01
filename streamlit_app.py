b6ef0b5c2cf4e9fe0a8cb182fa7786e05f8d810d
AbhishekPareek2005 <135148917+AbhishekPareek2005@users.noreply.github.com>
2023-06-01 05:37:39 +0530

    Update streamlit_app.py - added emojis and new item
    added emojis and a new item boiled eggs to the menu code.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 streamlit.title('My parents new healthy dinner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Masala Dosa')
-streamlit.text('Poha')
-streamlit.text('Aloo Parantha')
+streamlit.text('🍞 Masala Dosa')
+streamlit.text('🥣 Poha')
+streamlit.text('🍞 Aloo Parantha')
+Streamlit.text('🥑 🥗 Salad and Soup')
+Streamlit.text('🐔 boiled eggs')
