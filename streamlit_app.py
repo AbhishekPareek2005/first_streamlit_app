4ed1462ff644f2e2cc11319807172ed6a693c8b8
AbhishekPareek2005 <135148917+AbhishekPareek2005@users.noreply.github.com>
2023-06-01 05:44:01 +0530

    Update streamlit_app.py - spelling correction and header addition
    line 2 : changed dinner to diner
    line 9 : added new code line streamlit.header....

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 import streamlit
-streamlit.title('My parents new healthy dinner')
+streamlit.title('My parents new healthy diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('🍞 Masala Dosa')
 streamlit.text('🥣 Poha')
 streamlit.text('🍞 Aloo Parantha')
 streamlit.text('🥑🥗 Salad and Soup')
 streamlit.text('🐔 boiled eggs')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
